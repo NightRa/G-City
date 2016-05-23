c30ba8f85fce3ef8c1212ff3b884a2e0a7ab2025
Gil Henkin <gil7788@gmail.com>
2016-05-23 15:56:17 +0300

    Start implementing functions

--- BigData3/final-project/src/main/scala/univ/bigdata/course/part1/MoviesFunctions.scala
+++ BigData3/final-project/src/main/scala/univ/bigdata/course/part1/MoviesFunctions.scala
@@ -1,8 +1,23 @@
 package univ.bigdata.course.part1
 
+import org.apache.spark.rdd.RDD
+import univ.bigdata.course.part1.movie.Movie
+import univ.bigdata.course.util.Doubles._
+
 object MoviesFunctions {
   // Can have as input whatever you need.
   // Examples:
   //   movies: RDD[Movie]
   //   moviesById: RDD[(String, Movie)]
+
+  def totalMoviesAverageScore(movies: RDD[Movie]) = {
+    round(movies.flatMap(_.movieReviews.map(_.score)).mean())
+  }
+
+  def totalMovieAverage(moviesById: RDD[(String, Movie)], productId: String): Double = {
+    val maybeMovie: Seq[Movie] = moviesById.lookup(productId)
+    if (maybeMovie.isEmpty) -1
+    else
+      round(maybeMovie.head.avgScore)
+  }
 }
