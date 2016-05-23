142c478fb9cdf2091833da5f9c759fb21a17f30c
NightRa <ilan3580@gmail.com>
2016-05-23 13:35:16 +0300

    Command datatypes

---
+++ BigData3/final-project/src/main/scala/univ/bigdata/course/Command.scala
@@ -0,0 +1,14 @@
+package univ.bigdata.course
+
+sealed trait Command
+case object TotalMoviesAverageScore
+case class TotalMovieAverage(productID: String)
+case class GetTopKMoviesAverage(topK: Int)
+case object MovieWithHighestAverage
+case object MostReviewedProduct
+case class ReviewCountPerMovieTopKMovies(topK: Int)
+case class MostPopularMovieReviewedByKUsers(numOfUsers: Int)
+case class MoviesReviewWordsCount(topK: Int)
+case class TopYMoviesReviewTopXWordsCount(topMovies: Int, topWords: Int)
+case class TopKHelpfulUsers(k: Int)
+case object MoviesCount
