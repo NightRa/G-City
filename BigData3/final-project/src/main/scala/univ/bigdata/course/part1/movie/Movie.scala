293fc84427ab5ffdcd609a83e81d0c6b94f87dac
NightRa <ilan3580@gmail.com>
2016-05-23 15:46:38 +0300

    Use Vector as movieReviews

--- BigData3/final-project/src/main/scala/univ/bigdata/course/part1/movie/Movie.scala
+++ BigData3/final-project/src/main/scala/univ/bigdata/course/part1/movie/Movie.scala
@@ -1,19 +1,15 @@
 package univ.bigdata.course.part1.movie
 
-import org.apache.spark.rdd.RDD
-import univ.bigdata.course.util.Lazy
-import org.apache.spark.SparkContext._
 import univ.bigdata.course.util.Doubles._
 
-case class Movie(movieId: String, private val _movieReviews: RDD[MovieReview]) {
+case class Movie(movieId: String, movieReviews: Vector[MovieReview]) {
   /**
     * The reviews for the given movie.
     * Invariant: all the reviews should have the @movieId field equal to the Movie's @movieId field.
     * Invariant: Must be non empty!
     **/
-  val movieReviews = _movieReviews.cache()
 
-  lazy val avgScore = movieReviews.map(_.score).mean()
+  lazy val avgScore = movieReviews.view.map(_.score).sum / movieReviews.size
 
   override def toString: String =
     "Movie{" +
