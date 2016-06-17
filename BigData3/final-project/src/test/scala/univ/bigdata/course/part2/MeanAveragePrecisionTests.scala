f656b3258ca32087d529559729a908921987a58f
YuvalAlf <u67v67@gmail.com>
2016-06-17 15:10:20 +0300

    Part3 tests - epsilon for floating point accuracy

--- BigData3/final-project/src/test/scala/univ/bigdata/course/part2/MeanAveragePrecisionTests.scala
+++ BigData3/final-project/src/test/scala/univ/bigdata/course/part2/MeanAveragePrecisionTests.scala
@@ -13,7 +13,7 @@ class MeanAveragePrecisionTests {
 
   @Test
   def test1 : Unit = {
-    val rankVector1 = Vector(0,1,2)
+    val rankVector1 = Vector(1,2).map(_-1)
     val expectedMapValue = 1.0
     val actualMapValue =  Map.calcMap(asRDD(rankVector1))
     Assert.assertEquals(expectedMapValue, actualMapValue, Epsilon)
@@ -21,9 +21,9 @@ class MeanAveragePrecisionTests {
 
   @Test
   def test2 : Unit = {
-    val rankVector1 = Vector(0,1,2)
-    val rankVector2 = Vector(0,1,2,3,4,5,6,7,8,9,10,11,12)
-    val rankVector3 = Vector(0,1,2,3,4,5,6)
+    val rankVector1 = Vector(1,2,3).map(_-1)
+    val rankVector2 = Vector(1,2,3,4,5,6,7,8,9,10,11,12).map(_-1)
+    val rankVector3 = Vector(1,2,3,4,5,6).map(_-1)
     val expectedMapValue = 1.0
     val actualMapValue =  Map.calcMap(asRDD(rankVector1, rankVector2, rankVector3))
     Assert.assertEquals(expectedMapValue, actualMapValue, Epsilon)
@@ -32,7 +32,7 @@ class MeanAveragePrecisionTests {
   @Test
   def test3 : Unit = {
     val rankVector1 = Vector()
-    val rankVector2 = Vector(0,1,2,3,4,5,6,7,8,9,10,11,12)
+    val rankVector2 = Vector(1,2,3,4,5,6,7,8,9,10,11,12).map(_-1)
     val rankVector3 = Vector()
     val expectedMapValue = 1.0
     val actualMapValue =  Map.calcMap(asRDD(rankVector1, rankVector2, rankVector3))
@@ -46,4 +46,16 @@ class MeanAveragePrecisionTests {
     val actualMapValue =  Map.calcMap(asRDD(rankVector1, rankVector2))
     Assert.assertEquals(expectedMapValue, actualMapValue, Epsilon)
   }
+  @Test
+  def test5 : Unit = {
+    val rankVector1 = Vector(1,3,5,6,7,8,9,13).map(_-1)  // (1/1 + 2/3 + 3/5 + 4/6 + 5/7 + 6/8 + 7/9 + 8/13) / 8
+    val rankVector2 = Vector()
+    val rankVector3 = Vector(2,5,7,9,100,102).map(_-1)   // (1/2 + 2/5 + 3/7 + 4/9 + 5/100 +6/102) / 6
+    val rankVector4 = Vector()
+    val rankVector5 = Vector(4,6,8,9,12,14,22).map(_-1)  // (1/4 + 2/6 + 3/8 + 4/9 + 5/12 + 6/14 + 7/22) / 7
+    val expectedMapValue = (0.72384768009 + 0.3136399005 + 0.36659967017) / 3.0
+    val actualMapValue =  Map.calcMap(asRDD(rankVector1, rankVector2, rankVector3, rankVector4, rankVector5))
+    Assert.assertEquals(expectedMapValue, actualMapValue, Epsilon)
+  }
+  
 }
