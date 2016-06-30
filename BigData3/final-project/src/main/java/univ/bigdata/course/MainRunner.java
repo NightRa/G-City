665457483bd85b1697be3e1cbf2986592891b129
NightRa <ilan3580@gmail.com>
2016-06-29 21:22:51 -0700

    Add run command example, get real args

--- BigData3/final-project/src/main/java/univ/bigdata/course/MainRunner.java
+++ BigData3/final-project/src/main/java/univ/bigdata/course/MainRunner.java
@@ -3,9 +3,6 @@ package univ.bigdata.course;
 public class MainRunner {
 
     public static void main(String[] args) {
-        args = new String[2];
-        args[0] = "pagerank";
-        args[1] = "C:\\Users\\user\\BigData\\G-City\\BigData3\\BigDataset\\movies-simple4.txt";
         SparkMain.main(args);
     }
 }
